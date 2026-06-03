6a0ef6385c9311fad576488e38f14df574a3ed1d
brutals-dev <siddheshvikmalve@gmail.com>
2026-06-03 18:32:09 +0530

    Update partner.js

--- commands/commands/commands/partner.js
+++ commands/commands/commands/partner.js
@@ -1,20 +1,15 @@
-const { EmbedBuilder } = require("discord.js");
-
 module.exports = {
   name: "partner",
+
   execute(message) {
-
-    const embed = new EmbedBuilder()
-      .setColor("#f3d6df")
-      .setDescription(`💞 Partner system active!
-
-Step 1: Meet requirements
-Step 2: Send .ad
-Step 3: Send proof
-Step 4: Wait for approval`)
-      .setFooter({ text: "Partner System ♡" })
-      .setTimestamp();
-
-    message.channel.send({ embeds: [embed] });
+    message.channel.send({
+      content:
+        "💞 **Pascal Partnership System**\n\n" +
+        "① Meet requirements\n" +
+        "② Use `.ad` in your server\n" +
+        "③ Send uncropped proof\n" +
+        "④ Wait for approval\n\n" +
+        "✨ Good luck!"
+    });
   }
 };
