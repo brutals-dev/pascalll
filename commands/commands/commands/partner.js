6e086dcd459518561d3e1d1c05403952b3ab011e
brutals-dev <siddheshvikmalve@gmail.com>
2026-06-03 18:10:40 +0530

    Refactor partner command to use EmbedBuilder

--- commands/commands/commands/partner.js
+++ commands/commands/commands/partner.js
@@ -1,73 +1,20 @@
-const {
+const { EmbedBuilder } = require("discord.js");
 
-EmbedBuilder
+module.exports = {
+  name: "partner",
+  execute(message) {
 
-}=require(
+    const embed = new EmbedBuilder()
+      .setColor("#f3d6df")
+      .setDescription(`💞 Partner system active!
 
-"discord.js"
+Step 1: Meet requirements
+Step 2: Send .ad
+Step 3: Send proof
+Step 4: Wait for approval`)
+      .setFooter({ text: "Partner System ♡" })
+      .setTimestamp();
 
-);
-
-const config=require(
-
-"../config.json"
-
-);
-
-module.exports={
-
-name:"partner",
-
-execute(message){
-
-const embed=
-
-new EmbedBuilder()
-
-.setColor(
-
-"#f3d6df"
-
-)
-
-.setDescription(
-
-`💞 **tysm for partnering with Pascal !!**
-
-**① Meet our requirements**
-
-→ <#${config.requirementsChannel}>
-
-**② Type \`.ad\`**
-
-Send our advertisement inside your server
-
-**③ Send proof**
-
-Take an UNCROPPED screenshot
-
-**④ Wait patiently**
-
-Staff will review your partnership ✨`
-
-)
-
-.setFooter({
-
-text:
-
-"Pascal Partnership System ♡"
-
-})
-
-.setTimestamp();
-
-message.channel.send({
-
-embeds:[embed]
-
-});
-
-}
-
+    message.channel.send({ embeds: [embed] });
+  }
 };
