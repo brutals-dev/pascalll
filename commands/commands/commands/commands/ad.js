9cbaac7cbc2e47bc7136840d06a6e14edcab4821
brutals-dev <siddheshvikmalve@gmail.com>
2026-06-03 18:39:35 +0530

    Update ad.js

--- commands/commands/commands/commands/ad.js
+++ commands/commands/commands/commands/ad.js
@@ -1,9 +1,6 @@
 module.exports = {
   name: "ad",
-
   execute(message) {
-    message.channel.send({
-      content: "✦ PASCAL CLOTHING AD ✦\n\n✨ Premium Roblox Clothing\n🎁 Weekly Giveaways\n💞 Active Community\n\ndiscord.gg/pascal"
-    });
+    message.channel.send("✦ PASCAL CLOTHING AD WORKS ✅");
   }
 };
