40ebcfadfc01c04c90004b0cf023435eef4625c8
brutals-dev <siddheshvikmalve@gmail.com>
2026-06-03 18:40:33 +0530

    Update partner.js

--- commands/commands/commands/partner.js
+++ commands/commands/commands/partner.js
@@ -1,15 +1,6 @@
 module.exports = {
   name: "partner",
-
   execute(message) {
-    message.channel.send({
-      content:
-        "💞 **Pascal Partnership System**\n\n" +
-        "① Meet requirements\n" +
-        "② Use `.ad` in your server\n" +
-        "③ Send uncropped proof\n" +
-        "④ Wait for approval\n\n" +
-        "✨ Good luck!"
-    });
+    message.channel.send("💞 PARTNER SYSTEM WORKS ✅");
   }
 };
