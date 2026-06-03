1549bb87b9930dc8da867220c3df0d4a5f8eb1f0
brutals-dev <siddheshvikmalve@gmail.com>
2026-06-03 17:52:30 +0530

    Update ad.js

--- commands/commands/commands/commands/ad.js
+++ commands/commands/commands/commands/ad.js
@@ -1,89 +1,38 @@
-const {
+const { EmbedBuilder } = require("discord.js");
 
-EmbedBuilder
+module.exports = {
+  name: "ad",
+  execute(message) {
 
-}=require(
-
-"discord.js"
-
-);
-
-module.exports={
-
-name:"ad",
-
-execute(message){
-
-const embed=
-
-new EmbedBuilder()
-
-.setColor(
-
-"#f3d6df"
-
-)
-
-.setTitle(
-
-"✦ PASCAL CLOTHING ✦"
-
-)
-
-.setDescription(
-
-`> The New Generation Of Clothing
+    const embed = new EmbedBuilder()
+      .setColor("#f3d6df")
+      .setTitle("✦ PASCAL CLOTHING ✦")
+      .setDescription(`> The New Generation Of Clothing
 
 🛍️ Premium Roblox Clothing
-
 ✦ Original Designs
-
 ✦ Frequent Drops
-
 ✦ Clean & Unique Fits
-
 ✦ Styles For Every Aesthetic
 
 🎁 Weekly Giveaways
-
 ✦ 300 Robux Every Week
 
 🌟 Why Join?
-
 ✦ Active Community
-
 ✦ Sneak Peeks & Events
-
 ✦ Exclusive Content
 
 > Upgrade Your Avatar
-
 > Discover Your Next Favorite Fit
 
 ✦ Pascal Clothing
-
 「 Style Starts Here 」
 
-discord.gg/pascal`
+discord.gg/pascal`)
+      .setFooter({ text: "thank you for supporting Pascal ♡" })
+      .setTimestamp();
 
-)
-
-.setFooter({
-
-text:
-
-"thank you for supporting Pascal ♡"
-
-})
-
-.setTimestamp();
-
-message.channel.send({
-
-embeds:[embed]
-
-});
-
-}
-
+    message.channel.send({ embeds: [embed] });
+  }
 };
